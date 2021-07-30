ee6e2e11e5dc541a1967f72f7317c5f4184b5a2e
wolf2000-hub <alirezakn@outlook.com>
2021-07-30 23:51:38 +0430

    first change

---
+++ src/Auth/login.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Auth.css"
+// import styled from "style-components";
+const Login = (props) => {
+    return (
+        <main >
+            <nav >
+                <a href="/">
+                    <img className="w-100 h-100"  src="/images/images-removebg-preview.png" alt="" />
+                </a>
+                <div className="nav_join">
+               join now
+                </div>
+                </nav>
+        </main>
+    )
+};
+// const Container = styled.div``;
+// const Nav = styled.nav``;
+export default Login;
